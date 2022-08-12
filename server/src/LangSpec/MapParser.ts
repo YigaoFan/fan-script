0a977d699ecf965b368f3a276f9724205ff62e26
YigaoFan <fanyigao@qq.com>
2022-08-12 09:19:56 +0800

    add chart parser part code

--- server/src/LangSpec/MapParser.ts
+++ server/src/LangSpec/MapParser.ts
@@ -1,3 +1,7 @@
+import { IInputStream, IParser, ParserInput, ParserResult } from "../IParser";
+import { ISyntaxNode } from "../ISyntaxNode";
+import { Expression } from "./Expression";
+
 // TODO add space
 const grammarMap: (readonly [string, string[]])[] = [
     ['exp', ['literal']],
@@ -33,3 +37,63 @@ const grammarMap: (readonly [string, string[]])[] = [
     // func要用这种方法定义吗，还是保持原来的
     // 感觉可以调用原来的，要有个注册机制，只要把 func 的 parser 注册为原来的就行
 ];
+
+class ParserState {
+    public From: number;
+    public Rule: readonly [string, string[]];
+    /** now on @property Rule[NowPoint] left */
+    public NowPoint: number;
+
+    public static New(from: number, rule: readonly [string, string[]], nowPoint: number) {
+        return new ParserState(from, rule, nowPoint);
+    }
+
+    private constructor(from: number, rule: readonly [string, string[]], nowPoint: number) {
+        this.From = from;
+        this.Rule = rule;
+        this.NowPoint = nowPoint;
+    }    
+}
+
+class ExpressionChartParser implements IParser<Expression> {
+    private mStateChart: ParserState[][];
+    private mEndChar: string;
+
+    public constructor(endChar: string) {
+        this.mEndChar = endChar;
+        this.mStateChart = [];
+    }
+    // setup related parsers
+
+    // 要保存一些协程，因为有些函数可能解析到一半停下来，比如解析 id
+    // 那 stream.NextChar 就要加 async
+    public parse(input: ParserInput): ParserResult<Expression> {
+        const onZero: ParserState[] = [];
+        for (const i of grammarMap) {
+            onZero.push(ParserState.New(0, i, 0));
+        }
+        this.mStateChart.push(onZero);
+
+        for (;;) {
+            const r = this.iter(input);
+            if (r) {
+                break;
+            }
+        }
+        return null;
+    }
+
+    /**
+     * @returns end or not
+     */
+    public iter(input: ParserInput): boolean {
+        const c = input.NextChar;
+        if (c.Value === this.mEndChar) {
+            return true;
+        }
+        // closure
+        // shift
+        // reduction
+        return false;
+    }
+}
